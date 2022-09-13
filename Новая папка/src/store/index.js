304851bc306b6750488b11d51bfd38e7fa0c6d75
Vetrash <Vetrash47@gmail.com>
2022-09-13 13:10:00 +0300

    Revert "исправления замечаний"
    This reverts commit 41f66bc8d60f83567d1ca4cc797d2efcef58d482.

--- Новая папка/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReduser from './usersSlice.js'
-export default configureStore({
-    reducer: {
-        users: userReduser,
-    }
-});
